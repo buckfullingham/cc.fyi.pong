9a27d15c50eac269b79253dd0dd240259b2e3740
James Taylor <james@onalog.net>
2024-11-03 12:31:03 +0000

    add dear imgui

--- src/main/pong.cpp
+++ src/main/pong.cpp
@@ -1,186 +1,181 @@
-//
-// Book:      OpenGL(R) ES 2.0 Programming Guide
-// Authors:   Aaftab Munshi, Dan Ginsburg, Dave Shreiner
-// ISBN-10:   0321502795
-// ISBN-13:   9780321502797
-// Publisher: Addison-Wesley Professional
-// URLs:      http://safari.informit.com/9780321563835
-//            http://www.opengles-book.com
-//
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
 
-// Hello_Triangle.c
-//
-//    This is a simple example that draws a single triangle with
-//    a minimal vertex/fragment shader.  The purpose of this
-//    example is to demonstrate the basic concepts of
-//    OpenGL ES 2.0 rendering.
-#include "esUtil.h"
-#include <cstdlib>
-#include <memory>
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
 
-typedef struct {
-  // Handle to a program object
-  GLuint programObject;
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
-} UserData;
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
 
-///
-// Create a shader object, load the shader source, and
-// compile the shader.
-//
-GLuint LoadShader(GLenum type, const char *shaderSrc) {
-  GLuint shader;
-  GLint compiled;
+// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
+#ifdef __EMSCRIPTEN__
+#include "emscripten_mainloop_stub.h"
+#endif
 
-  // Create the shader object
-  shader = glCreateShader(type);
+static void glfw_error_callback(int error, const char* description)
+{
+  fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
 
-  if (shader == 0)
-    return 0;
+// Main code
+int main(int, char**)
+{
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit())
+    return 1;
 
-  // Load the shader source
-  glShaderSource(shader, 1, &shaderSrc, NULL);
+  // GL ES 2.0 + GLSL 100
+  const char* glsl_version = "#version 100";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 
-  // Compile the shader
-  glCompileShader(shader);
+  // Create window with graphics context
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "PONG", nullptr, nullptr);
+  if (window == nullptr)
+    return 1;
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1); // Enable vsync
 
-  // Check the compile status
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
-  if (!compiled) {
-    GLint infoLen = 0;
+  ImGui::StyleColorsDark();
 
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplGlfw_InstallEmscriptenCallbacks(window, "#canvas");
+  ImGui_ImplOpenGL3_Init(glsl_version);
 
-    if (infoLen > 1) {
-      std::unique_ptr<char[]> infoLog(new char[infoLen]);
+  // Load Fonts
+  // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+  // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+  // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+  // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+  // - Read 'docs/FONTS.md' for more instructions and details.
+  // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+  // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
+  //io.Fonts->AddFontDefault();
+  //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+  //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+  //IM_ASSERT(font != nullptr);
 
-      glGetShaderInfoLog(shader, infoLen, NULL, infoLog.get());
-      esLogMessage("Error compiling shader:\n%s\n", infoLog.get());
+  // Our state
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  // Main loop
+#ifdef __EMSCRIPTEN__
+//   For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+//   You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+//  io.IniFilename = nullptr;
+  EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+  while (!glfwWindowShouldClose(window))
+#endif
+  {
+    // Poll and handle events (inputs, window resize, etc.)
+    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+    glfwPollEvents();
+    if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
+    {
+      ImGui_ImplGlfw_Sleep(10);
+      continue;
     }
 
-    glDeleteShader(shader);
-    return 0;
-  }
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
-  return shader;
-}
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    if (show_demo_window)
+      ImGui::ShowDemoWindow(&show_demo_window);
 
-///
-// Initialize the shader and program object
-//
-int Init(ESContext *esContext) {
-  esContext->userData = malloc(sizeof(UserData));
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+    {
+      static float f = 0.0f;
+      static int counter = 0;
 
-  UserData *userData = static_cast<UserData *>(esContext->userData);
-  GLbyte vShaderStr[] = "attribute vec4 vPosition;    \n"
-                        "void main()                  \n"
-                        "{                            \n"
-                        "   gl_Position = vPosition;  \n"
-                        "}                            \n";
+      ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-  GLbyte fShaderStr[] = "precision mediump float;\n"
-                        "void main()                                  \n"
-                        "{                                            \n"
-                        "  gl_FragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );\n"
-                        "}                                            \n";
+      ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+      ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+      ImGui::Checkbox("Another Window", &show_another_window);
 
-  GLuint vertexShader;
-  GLuint fragmentShader;
-  GLuint programObject;
-  GLint linked;
+      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+      ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-  // Load the vertex/fragment shaders
-  vertexShader =
-      LoadShader(GL_VERTEX_SHADER, reinterpret_cast<const char *>(vShaderStr));
-  fragmentShader = LoadShader(GL_FRAGMENT_SHADER,
-                              reinterpret_cast<const char *>(fShaderStr));
+      if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+        counter++;
+      ImGui::SameLine();
+      ImGui::Text("counter = %d", counter);
 
-  // Create the program object
-  programObject = glCreateProgram();
-
-  if (programObject == 0)
-    return 0;
-
-  glAttachShader(programObject, vertexShader);
-  glAttachShader(programObject, fragmentShader);
-
-  // Bind vPosition to attribute 0
-  glBindAttribLocation(programObject, 0, "vPosition");
-
-  // Link the program
-  glLinkProgram(programObject);
-
-  // Check the link status
-  glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
-
-  if (!linked) {
-    GLint infoLen = 0;
-
-    glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &infoLen);
-
-    if (infoLen > 1) {
-      std::unique_ptr<char[]> infoLog(new char[infoLen]);
-
-      glGetProgramInfoLog(programObject, infoLen, NULL, infoLog.get());
-      esLogMessage("Error linking program:\n%s\n", infoLog.get());
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+      ImGui::End();
     }
 
-    glDeleteProgram(programObject);
-    return GL_FALSE;
+    // 3. Show another simple window.
+    if (show_another_window)
+    {
+      ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+      ImGui::Text("Hello from another window!");
+      if (ImGui::Button("Close Me"))
+        show_another_window = false;
+      ImGui::End();
+    }
+
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
   }
+#ifdef __EMSCRIPTEN__
+  EMSCRIPTEN_MAINLOOP_END;
+#endif
 
-  // Store the program object
-  userData->programObject = programObject;
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
 
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-  return GL_TRUE;
-}
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
-///
-// Draw a triangle using the shader pair created in Init()
-//
-void Draw(ESContext *esContext) {
-  UserData *userData = static_cast<UserData *>(esContext->userData);
-  GLfloat vVertices[] = {0.0f, 0.5f, 0.0f,  -0.5f, -0.5f,
-                         0.0f, 0.5f, -0.5f, 0.0f};
-
-  // No clientside arrays, so do this in a webgl-friendly manner
-  GLuint vertexPosObject;
-  glGenBuffers(1, &vertexPosObject);
-  glBindBuffer(GL_ARRAY_BUFFER, vertexPosObject);
-  glBufferData(GL_ARRAY_BUFFER, 9 * 4, vVertices, GL_STATIC_DRAW);
-
-  // Set the viewport
-  glViewport(0, 0, esContext->width, esContext->height);
-
-  // Clear the color buffer
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  // Use the program object
-  glUseProgram(userData->programObject);
-
-  // Load the vertex data
-  glBindBuffer(GL_ARRAY_BUFFER, vertexPosObject);
-  glVertexAttribPointer(0 /* ? */, 3, GL_FLOAT, 0, 0, 0);
-  glEnableVertexAttribArray(0);
-
-  glDrawArrays(GL_TRIANGLES, 0, 3);
-}
-
-int main(int, char *[]) {
-  ESContext esContext;
-  UserData userData;
-
-  esInitContext(&esContext);
-  esContext.userData = &userData;
-
-  esCreateWindow(&esContext, "Hello Triangle", 320, 240, ES_WINDOW_RGB);
-
-  if (!Init(&esContext))
-    return 0;
-
-  esRegisterDrawFunc(&esContext, Draw);
-
-  esMainLoop(&esContext);
+  return 0;
 }
